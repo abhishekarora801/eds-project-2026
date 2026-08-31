7018aa6dbc06ef9e6e823bc770c96003a599bf88
Abhishek Arora <aarora6@woolworths.com.au>
2026-08-31 23:04:16 +0530

    Revert "feat: add vertical variant to image-compare block"
    This reverts commit eddb5e0e923683bf67a24350a2061e0afbe5bf0e.

--- blocks/image-compare/image-compare.js
+++ blocks/image-compare/image-compare.js
@@ -1,5 +1,4 @@
 export default function decorate(block) {
-  const isVertical = block.classList.contains('vertical');
   const [right, left] = block.querySelectorAll(':scope > div');
   right.className = 'image-compare-right';
   left.className = 'image-compare-left';
@@ -13,14 +12,9 @@ export default function decorate(block) {
   range.max = 100;
   range.value = 50;
   range.setAttribute('aria-label', 'Compare images');
-  if (isVertical) range.setAttribute('orient', 'vertical');
 
   range.addEventListener('input', (e) => {
-    if (isVertical) {
-      left.style.height = `${e.target.value}%`;
-    } else {
-      left.style.width = `${e.target.value}%`;
-    }
+    left.style.width = `${e.target.value}%`;
   });
 
   slider.append(range);
